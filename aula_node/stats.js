f4c2afb1e04c2a113cd1e711fab7eee079f581a0
erika-porto <erikaportoharada@gmail.com>
2022-08-22 17:45:39 -0300

    practice of node class

---
+++ aula_node/stats.js
@@ -0,0 +1,26 @@
+const os = require('os');
+
+setInterval(() => {
+
+    const { freemem, totalmem } = os;
+
+    const total = parseInt(totalmem() / 1024 / 1024);
+    const mem = parseInt(freemem() / 1024 / 1024);
+    const percents = parseInt(mem / total * 100);
+
+    console.log(mem, total, percents);
+
+    
+    const stats = {
+        free: `${mem} MB`,
+        total: `${total} MB`,
+        usage: `${percents} %`
+    };
+
+    console.clear();
+    console.table(stats);
+    
+},1000);
+
+
+
